4f4007bd6db48b8be33084437c1053ed0d54fccc
Travis Hoover <traviskhoover@gmail.com>
2020-08-19 13:08:02 -0500

    serve html file. install socket.io

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3030, () => {
-  console.log('listening on *:3030');
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
 });
